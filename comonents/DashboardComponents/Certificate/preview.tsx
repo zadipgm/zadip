09928af17386c945c68dd770a77da9515c4d9476
zadipgm <gm.zadip@gmail.com>
2023-05-09 13:53:47 +0300

    update certificate fonts

--- comonents/DashboardComponents/Certificate/preview.tsx
+++ comonents/DashboardComponents/Certificate/preview.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { PreviewCertificate, QRcode } from "./styled";
+import { ContainerPdf, PreviewCertificate } from "./styled";
 import {
   Document,
   Page,
@@ -12,118 +12,134 @@ import {
 } from "@react-pdf/renderer";
 import { useRouter } from "next/router";
 
-const styles = StyleSheet.create({
-  page: {
-    // backgroundColor: "#d11fb6",
-  },
-  image: {
-    width: "auto",
-    height: "auto",
-  },
-  image2: {
-    width: 100,
-    height: 100,
-    position: "absolute",
-    left: "42%",
-    bottom: 0,
-    right: "42%",
-  },
-  section: {
-    margin: 10,
-    padding: 10,
-  },
-  viewer: {
-    width: "100%", //the pdf viewer will take up all of the width and height
-    height: "100vh",
-  },
-  ID: {
-    fontSize: 10,
-    color: "#465364",
-    position: "absolute",
-    left: "35%",
-    top: "34%",
-  },
-  certificate: {
-    fontSize: 10,
-    color: "#465364",
-    position: "absolute",
-    left: "35%",
-    top: "38.5%",
-  },
-  date: {
-    fontSize: 10,
-    color: "#465364",
-    position: "absolute",
-    left: "35%",
-    top: "43%",
-  },
-  name: {
-    fontSize: 10,
-    color: "#465364",
-    position: "absolute",
-    left: "20%",
-    top: "55%",
-  },
-  IDArabic: {
-    fontSize: 10,
-    color: "#465364",
-    position: "absolute",
-    right: "35%",
-    top: "34%",
-  },
-  certificateArabic: {
-    fontSize: 10,
-    color: "#465364",
-    position: "absolute",
-    right: "35%",
-    top: "38.5%",
-  },
-  dateArabic: {
-    fontSize: 10,
-    color: "#465364",
-    position: "absolute",
-    right: "35%",
-    top: "43%",
-  },
-  nameArabic: {
-    fontSize: 10,
-    color: "#465364",
-    position: "absolute",
-    right: "20%",
-    top: "55%",
-  },
-});
+// AIzaSyCoEVYGBZPtlTkNl7w11B6oz0G14R83FOg
 const PreViewCertificate = () => {
+  Font.register({
+    family: "Cairo",
+    fonts: [
+      {
+        src: "http://fonts.gstatic.com/s/cairo/v28/SLXgc1nY6HkvangtZmpQdkhzfH5lkSs2SgRjCAGMQ1z0hD45W1ToLQ-HmkA.ttf",
+      },
+    ],
+  });
+  const styles = StyleSheet.create({
+    page: {},
+    image: {
+      width: "98.5%",
+      height: "auto",
+    },
+    image2: {
+      width: 100,
+      height: 100,
+      position: "absolute",
+      left: "42%",
+      bottom: 0,
+      right: "42%",
+    },
+    section: {
+      margin: 10,
+      padding: 10,
+    },
+    viewer: {
+      width: "100%",
+      height: "100vh",
+    },
+    ID: {
+      fontSize: 14,
+      color: "#465364",
+      position: "absolute",
+      left: "29%",
+      top: "32%",
+    },
+    certificate: {
+      fontSize: 14,
+      color: "#465364",
+      position: "absolute",
+      left: "29%",
+      top: "36.5%",
+    },
+    date: {
+      fontSize: 14,
+      color: "#465364",
+      position: "absolute",
+      left: "29%",
+      top: "41%",
+      fontFamily: "Cairo",
+    },
+    name: {
+      fontSize: 14,
+      color: "#465364",
+      position: "absolute",
+      left: "18%",
+      top: "54.5%",
+    },
+    IDArabic: {
+      fontSize: 14,
+      color: "#465364",
+      position: "absolute",
+      right: "29%",
+      top: "32%",
+      fontFamily: "Cairo",
+    },
+    certificateArabic: {
+      fontSize: 14,
+      color: "#465364",
+      position: "absolute",
+      right: "29%",
+      top: "36.5%",
+      fontFamily: "Cairo",
+    },
+    dateArabic: {
+      fontSize: 14,
+      color: "#465364",
+      position: "absolute",
+      right: "29%",
+      top: "41%",
+      fontFamily: "Cairo",
+    },
+    nameArabic: {
+      fontSize: 14,
+      color: "#465364",
+      position: "absolute",
+      right: "20%",
+      top: "53.5%",
+      fontFamily: "Cairo",
+    },
+  });
+  React.useEffect(() => {
+    setTimeout(() => {
+      pdfTemplate();
+    }, 3000);
+  }, []);
   const router = useRouter();
-  return (
-    <PDFViewer style={styles.viewer}>
-      <Document language="ar">
-        <Page size="A4" style={styles.page} orientation="landscape">
-          <View style={styles.section}>
-            <Image style={styles.image} src="/images/certificate.jpeg" />
-            <Text style={styles.ID}>{router.query.idnumber}</Text>
-            <Text style={styles.certificate}>
-              {router.query.certificate_number}
-            </Text>
-            <Text style={styles.date}>{router.query.expire_date}</Text>
-            <Text style={styles.name}>{router.query.name}</Text>
-            <Text style={styles.IDArabic}>{router.query.idnumberArabic}</Text>
-            <Text style={styles.certificateArabic}>
-              {router.query.certificate_numberArabic}
-            </Text>
-            <Text style={styles.dateArabic}>
-              {router.query.expire_dateArabic}
-            </Text>
-            <Text style={styles.nameArabic}>{router.query.name}</Text>
-            {/* <QRcode>
-              <Image src="/images/qrcode.png" style={styles.image2} />
-            </QRcode> */}
-            <PreviewCertificate>Preview Certificate</PreviewCertificate>
-          </View>
-        </Page>
-      </Document>
-    </PDFViewer>
-  );
+  const pdfTemplate = () => {
+    return (
+      <PDFViewer style={styles.viewer} fileName={"certificate"}>
+        <Document language="ar">
+          <Page size="A4" style={styles.page} orientation="landscape">
+            <View style={styles.section}>
+              <Image style={styles.image} src="/images/certificate.jpeg" />
+              <Text style={styles.ID}>{router.query.idnumber}</Text>
+              <Text style={styles.certificate}>
+                {router.query.certificate_number}
+              </Text>
+              <Text style={styles.date}>{router.query.expire_date}</Text>
+              <Text style={styles.name}>{router.query.name}</Text>
+              <Text style={styles.IDArabic}>{router.query.idnumberArabic}</Text>
+              <Text style={styles.certificateArabic}>
+                {router.query.certificate_numberArabic}
+              </Text>
+              <Text style={styles.dateArabic}>
+                {router.query.expire_dateArabic}
+              </Text>
+              <Text style={styles.nameArabic}>{router.query.nameArabic}</Text>
+            </View>
+          </Page>
+        </Document>
+      </PDFViewer>
+    );
+  };
+  return <ContainerPdf>{pdfTemplate()}</ContainerPdf>;
 };
-
+// ReactPDF.render(<PreViewCertificate />, "certificate.pdf");
 export default PreViewCertificate;
