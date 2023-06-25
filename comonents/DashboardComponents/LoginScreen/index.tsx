f44f646ccb451edcd1382775ba4ef4ca89fa62fe
zadipgm <gm.zadip@gmail.com>
2023-06-25 12:59:34 +0300

    ./ error

--- comonents/DashboardComponents/LoginScreen/index.tsx
+++ comonents/DashboardComponents/LoginScreen/index.tsx
@@ -80,7 +80,7 @@ const LoginScreen = () => {
       setIsComplete(true);
       setTimeout(function () {
         setIsComplete(false);
-        router.push(`dashboard`);
+        router.push(`${locale}/dashboard/`);
       }, 3000);
       setColor("#0d880d");
     } catch (error) {
