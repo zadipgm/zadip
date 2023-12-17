d7f0eee5aafb572eff4cbdcb7febc6aab55e4c45
zadipgm <gm.zadip@gmail.com>
2023-12-17 07:22:45 +0000

    merge conflicts

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,19 +1,9 @@
 module.exports = {
   apps: [
     {
-<<<<<<< HEAD
       name: 'next-app',
       script: 'node_modules/next/dist/bin/next',
       args: 'start',
     },
   ],
 };
-
-=======
-      name: "next-app",
-      script: "node_modules/next/dist/bin/next",
-      args: "start",
-    },
-  ],
-};
->>>>>>> e290c5989122f67110b1fc4d603bef08d66a8362
